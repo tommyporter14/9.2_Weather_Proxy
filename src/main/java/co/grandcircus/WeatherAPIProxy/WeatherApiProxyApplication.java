889db6c3e32fe73a477bfc0b7184669c9ccba57a
tommy <tommyporter14@gmail.com>
2022-08-17 15:37:53 -0400

    set up only

---
+++ src/main/java/co/grandcircus/WeatherAPIProxy/WeatherApiProxyApplication.java
@@ -0,0 +1,13 @@
+package co.grandcircus.WeatherAPIProxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApiProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherApiProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/grandcircus/WeatherAPIProxy/WeatherApiProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package co.grandcircus.WeatherAPIProxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApiProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
